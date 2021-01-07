b3f43530eb6e26db284c2d3c74dbba67172a758a
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-07 16:40:32 +0100

    Esercizio

--- js/script-paridispari.js
+++ js/script-paridispari.js
@@ -1,26 +1,29 @@
 
 // Scelta pari o dispari
-// var scelta = prompt('Pari o dispari?');
-// if (scelta !== 'pari' || scelta !== 'dispari') {
-//   alert('Qualcosa non ha funzionato')
-// }
+var scelta = prompt('Pari o dispari?');
+if (scelta === 'pari' || scelta === 'dispari') {
+  // Scelta numero
+  var numeroUtente = prompt('Scegliere un numero da 1 a 5');
+  if (numeroUtente > 5 || numeroUtente < 1){
+    alert('Numero non disponibile');
+  }
+  parseInt(numeroUtente);
 
-// Scelta numero
-var numeroUtente = prompt('Scegliere un numero da 1 a 5');
-if (numeroUtente > 5 || numeroUtente < 1){
-  alert('Numero non disponibile');
+  function randomNumber(min, max) {
+    var result = Math.floor(Math.random() * (max + 1 - min) + min);
+    console.log(result);
+    return result;
+
+  }
+
+  function add(num1, num2){
+    var somma = num1 + num2;
+    console.log(parseInt(somma));
+    return somma;
+  }
+  randomNumber(1, 5);
+  add(numeroUtente, result);
+
+}else {
+  alert('Qualcosa non ha funzionato');
 }
-
-function randomNumber(min, max) {
-  var result = Math.floor(Math.random() * (max + 1 - min) + min);
-  console.log(parseInt(result));
-  return result;
-
-}
-
-function add(num1, num2){
-  var somma = num1 + num2;
-  console.log(parseInt(somma));
-}
-randomNumber(1, 5);
-add(numeroUtente, result)
