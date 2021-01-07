19dcc34fd8209f6c8eb1ee22e40f7dfe0a94ade7
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-07 17:12:29 +0100

    Esercizio

--- js/script-paridispari.js
+++ js/script-paridispari.js
@@ -4,26 +4,34 @@ var scelta = prompt('Pari o dispari?');
 if (scelta === 'pari' || scelta === 'dispari') {
   // Scelta numero
   var numeroUtente = prompt('Scegliere un numero da 1 a 5');
+  console.log('Il numero scelto è: ' + numeroUtente);
   if (numeroUtente > 5 || numeroUtente < 1){
     alert('Numero non disponibile');
   }
-  parseInt(numeroUtente);
 
   function randomNumber(min, max) {
     var result = Math.floor(Math.random() * (max + 1 - min) + min);
-    console.log(result);
+    console.log('Il numero del computer è: ' + result);
     return result;
 
   }
 
   function add(num1, num2){
-    var somma = num1 + num2;
-    console.log(somma);
-    return somma;
-  }
-  randomNumber(1, 5);
+      var somma = parseInt(num1) + parseInt(num2);
+      console.log('Il risultato è: ' + somma);
+      return somma;
+    }
+
+  var result = randomNumber(1, 5);
   add(numeroUtente, result);
 
+  if (somma % 2 === 0 && scelta === 'pari') {
+    alert('Complimenti hai vinto');
+  }else if (somma % 2 !== 0 && scelta === 'dispari') {
+    alert('Complimenti hai vinto');
+  }else{
+    alert('Hai perso');
+  }
 }else {
   alert('Qualcosa non ha funzionato');
 }
